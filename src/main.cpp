7f3566a6c60e597d7814d3002bda2ed8565d9ba6
SkirriLEX <kirk.andri@gmail.com>
2020-11-22 18:43:18 +0200

    final ready tested relize last

--- src/main.cpp
+++ src/main.cpp
@@ -1,88 +1,89 @@
 #include <Arduino.h>
+
 #include "MPU9250.h"
+#include "BMP280_DEV.h"
 
-#include <BMP280_DEV.h> // Include the BMP280_DEV.h library
-
-BMP280_DEV bmp280; // Instantiate (create) a BMP280_DEV object and set-up for I2C operation (address 0x77)
-// an MPU9250 object with the MPU-9250 sensor on I2C bus 0 with address 0x68
-MPU9250 IMU(Wire, 0x68);
+BMP280_DEV bmp280;       //(address 0x77)
+MPU9250 MPU(Wire, 0x68); //(address 0x68)
 int status;
 
 void setup()
 {
-    // serial to display data
     Serial.begin(115200);
     while (!Serial)
     {
+        ;
     }
 
-    // start communication with IMU
     Wire.begin((uint8_t)PB11, (uint8_t)PB10);
-    status = IMU.begin();
+    status = MPU.begin();
+    Serial.println("MPU Sensor event test");
     if (status < 0)
     {
-        Serial.println("IMU initialization unsuccessful");
-        Serial.println("Check IMU wiring or try cycling power");
+        Serial.println("MPU initialization unsuccessful");
+        Serial.println("Check MPU wiring or try cycling power");
         Serial.print("Status: ");
         Serial.println(status);
-        status = IMU.begin();
-        delay(1000);
+        status = MPU.begin();
+        while (true)
+            ;
     }
     Serial.println(F("BMP280 Sensor event test"));
-    while (!bmp280.begin())
+    if (!bmp280.begin(0x76))
     {
         Serial.println(F("Could not find a valid BMP280 sensor, check wiring!"));
-        delay(1000);
+        while (true)
+            ;
     }
+    Wire.setClock(400000);
     bmp280.setPresOversampling(OVERSAMPLING_X1); // Set the pressure oversampling to X4
     bmp280.setTempOversampling(OVERSAMPLING_X1); // Set the temperature oversampling to X1
     bmp280.setIIRFilter(IIR_FILTER_OFF);         // Set the IIR filter to setting 4
     bmp280.startNormalConversion();              // Start BMP280 forced conversion (if we're in SLEEP_MODE)
 
-    // setting the accelerometer full scale range to +/-8G
-    IMU.setAccelRange(MPU9250::ACCEL_RANGE_8G);
-    // setting the gyroscope full scale range to +/-500 deg/s
-    IMU.setGyroRange(MPU9250::GYRO_RANGE_500DPS);
-    // setting DLPF bandwidth to 20 Hz
-    IMU.setDlpfBandwidth(MPU9250::DLPF_BANDWIDTH_20HZ);
-    // setting SRD to 19 for a 50 Hz update rate
-    IMU.setSrd(19);
+    MPU.setAccelRange(MPU9250::ACCEL_RANGE_16G);
+    MPU.setGyroRange(MPU9250::GYRO_RANGE_2000DPS);
+    MPU.setSrd(0);
+    MPU.enableDataReadyInterrupt();
+
+    Serial.println("init done");
 }
 
 void loop()
 {
-    // read the sensor
-    IMU.readSensor();
-
-    // display the data
-    Serial.print(IMU.getAccelX_mss(), 6);
-    Serial.print("\t");
-    Serial.print(IMU.getAccelY_mss(), 6);
-    Serial.print("\t");
-    Serial.print(IMU.getAccelZ_mss(), 6);
-    Serial.print("\t");
-    Serial.print(IMU.getGyroX_rads(), 6);
-    Serial.print("\t");
-    Serial.print(IMU.getGyroY_rads(), 6);
-    Serial.print("\t");
-    Serial.print(IMU.getGyroZ_rads(), 6);
-    Serial.print("\t");
-    Serial.print(IMU.getMagX_uT(), 6);
-    Serial.print("\t");
-    Serial.print(IMU.getMagY_uT(), 6);
-    Serial.print("\t");
-    Serial.print(IMU.getMagZ_uT(), 6);
-    Serial.print("\t");
-    Serial.println(IMU.getTemperature_C(), 6);
+    if (MPU.readSensor() > 0)
+    {
+        Serial.print(MPU._axcounts);
+        Serial.print("\t");
+        Serial.print(MPU._aycounts);
+        Serial.print("\t");
+        Serial.print(MPU._azcounts);
+        Serial.print("\t");
+        Serial.print(MPU._gxcounts);
+        Serial.print("\t");
+        Serial.print(MPU._gycounts);
+        Serial.print("\t");
+        Serial.print(MPU._gzcounts);
+        Serial.print("\t");
+        Serial.print(MPU._hxcounts);
+        Serial.print("\t");
+        Serial.print(MPU._hycounts);
+        Serial.print("\t");
+        Serial.print(MPU._hzcounts);
+        Serial.print("\t");
+        Serial.print(MPU._tcounts);
+        Serial.print("\t");
+        Serial.print(micros());
+        Serial.print("\n");
+    }
     float temperature, pressure;                   // Create the temperature, pressure and altitude variables
     if (bmp280.getTempPres(temperature, pressure)) // Check if the measurement is complete
     {
-        Serial.print(temperature); // Display the results
-        Serial.print(F("*C   "));
+        Serial.print(temperature);
+        Serial.print("\t");
         Serial.print(pressure);
-        Serial.print(F("hPa   "));
-        Serial.print(millis());
-        Serial.print(F("\n"));
+        Serial.print("\t");
+        Serial.print(micros());
+        Serial.print("\n");
     }
-    delay(1);
 }
